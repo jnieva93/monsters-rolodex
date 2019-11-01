dd7771dfa3d93db3ef518bb77b456fcdd22c8fb6
jnieva93 <josenieva93@gmail.com>
2019-11-01 10:26:14 -0300

    Updates

---
+++ precache-manifest.225d29c6da39f08b8ff2e709fdc5d262.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c39d0a13611a760a442e5d1e2e0153b6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "603824a70701f177566b",
+    "url": "/monsters-rolodex/static/css/main.95838695.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "603824a70701f177566b",
+    "url": "/monsters-rolodex/static/js/main.0a85c624.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
